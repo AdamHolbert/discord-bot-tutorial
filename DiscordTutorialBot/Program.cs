250af45ca16592c6cb447fe040ae8858a68e170c
Petr Sedláček <petr.spelos@gmail.com>
2018-04-14 20:09:52 +0200

    Add Console commands support

--- DiscordTutorialBot/Program.cs
+++ DiscordTutorialBot/Program.cs
@@ -32,7 +32,80 @@ namespace DiscordTutorialBot
             Global.Client = _client;
             _handler = new CommandHandler();
             await _handler.InitializeAsync(_client);
+            ConsoleInput();
             await Task.Delay(-1);
+        }
+
+        private async Task ConsoleInput()
+        {
+            var input = string.Empty;
+            while (input.Trim().ToLower() != "block")
+            {
+                input = Console.ReadLine();
+                if (input.Trim().ToLower() == "message")
+                    ConsoleSendMessage();
+            }
+        }
+
+        private async void ConsoleSendMessage()
+        {
+            Console.WriteLine("Select the guild:");
+            var guild = GetSelectedGuild(_client.Guilds);
+            var textChannel = GetSelectedTextChannel(guild.TextChannels);
+            var msg = string.Empty;
+            while (msg.Trim() == string.Empty)
+            {
+                Console.WriteLine("Your message:");
+                msg = Console.ReadLine();
+            }
+
+            await textChannel.SendMessageAsync(msg);
+        }
+
+        private SocketTextChannel GetSelectedTextChannel(IEnumerable<SocketTextChannel> channels)
+        {
+            var textChannels = channels.ToList();
+            var maxIndex = textChannels.Count - 1;
+            for (var i = 0; i <= maxIndex; i++)
+            {
+                Console.WriteLine($"{i} - {textChannels[i].Name}");
+            }
+
+            var selectedIndex = -1;
+            while (selectedIndex < 0 || selectedIndex > maxIndex)
+            {
+                var success = int.TryParse(Console.ReadLine().Trim(), out selectedIndex);
+                if (!success)
+                {
+                    Console.WriteLine("That was an invalid index, try again.");
+                    selectedIndex = -1;
+                }
+            }
+
+            return textChannels[selectedIndex];
+        }
+
+        private SocketGuild GetSelectedGuild(IEnumerable<SocketGuild> guilds)
+        {
+            var socketGuilds = guilds.ToList();
+            var maxIndex = socketGuilds.Count - 1;
+            for(var i = 0; i <= maxIndex; i++)
+            {
+                Console.WriteLine($"{i} - {socketGuilds[i].Name}");
+            }
+
+            var selectedIndex = -1;
+            while (selectedIndex < 0 || selectedIndex > maxIndex)
+            {
+                var success = int.TryParse(Console.ReadLine().Trim(), out selectedIndex);
+                if (!success)
+                {
+                    Console.WriteLine("That was an invalid index, try again.");
+                    selectedIndex = -1;
+                }
+            }
+
+            return socketGuilds[selectedIndex];
         }
 
         private async Task OnReactionAdded(Cacheable<IUserMessage, ulong> cache, ISocketMessageChannel channel, SocketReaction reaction)
